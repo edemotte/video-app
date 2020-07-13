dbdb072f84562146b27726b88f1fb1ebdd9a6cce
edemotte <eugenedelain@gmail.com>
2020-07-13 15:46:13 +0530

    Updates

---
+++ precache-manifest.0f6deccb6e28c63d60d935528f9359c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d6e9aa60bb0da2d06a70e869f4425b5",
+    "url": "/video-app/index.html"
+  },
+  {
+    "revision": "422db892483d2ffdbbce",
+    "url": "/video-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2da1b16b4d8bc2a69643",
+    "url": "/video-app/static/js/2.7ebaa6b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/video-app/static/js/2.7ebaa6b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "422db892483d2ffdbbce",
+    "url": "/video-app/static/js/main.c6a4469d.chunk.js"
+  },
+  {
+    "revision": "e21b1f0dacc29857f42b",
+    "url": "/video-app/static/js/runtime-main.20a2b6a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/video-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
